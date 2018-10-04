e69406e6932e120dbf56b4c367f8ec1769f4ab68
planavsa <anthony.planavsky@adp.com>
2018-10-03 23:47:14 -0400

    add welcome and tile components

---
+++ projects/components/src/lib/tile/tile.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TileComponent } from './tile.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TileComponent]
+})
+export class TileModule { }
